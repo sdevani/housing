b2909aa4887efcd7847b8cc04d102d0c14976087
Shehzan Devani <shehzandevani@gmail.com>
2016-02-24 14:58:54 -0800

    added support for ejs and nodemon

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('home');
+});
+
+app.post('/evaluate', function(req, res) {
+  res.send('Not functional yet!');
 });
 
 app.listen(port, function () {

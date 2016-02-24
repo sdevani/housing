a1ed1d957a3df0b490bc1ba0882ec6d65b780879
Shehzan Devani <shehzandevani@gmail.com>
2016-02-24 13:24:19 -0800

    Prepare for heroku

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Housing app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Housing app listening on port ' + port + '!');
 });
